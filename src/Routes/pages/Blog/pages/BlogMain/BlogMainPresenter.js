974a0b4be0bfaf92640070749c3a336a0955a60c
simonieee <75366051+simonieee@users.noreply.github.com>
2022-03-28 02:50:03 +0900

    [UPDATE] 메인 필터버튼 설정
    메인 필터버튼 설정

--- src/Routes/pages/Blog/pages/BlogMain/BlogMainPresenter.js
+++ src/Routes/pages/Blog/pages/BlogMain/BlogMainPresenter.js
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./BlogMian.css";
 import kakaoImg from "Assets/img/main/kakao_img.gif";
+import { TypeManager } from "Utils";
+
+const STATUS_LIST = [
+  {
+    id: "1",
+    type: "all",
+    value: "전체",
+    color: "#fff",
+    backgorundColor: "#6e95fd",
+  },
+  ...TypeManager.getMainFilterList(),
+];
 
 const BlogMainPresenter = () => {
+  /* Router */
+  /* State */
+  const [techList, setTechList] = useState("all");
+  /* Hooks */
+  /* Functions */
+  /**
+   * 버튼 상태변경
+   * @param {*} e
+   */
+  const handleTechList = (e) => {
+    setTechList(e.target.value);
+  };
+  /* Render */
+  /**
+   * 메인 필터 버튼 렌더
+   */
+  const buttonRedner = STATUS_LIST.map((item) => {
+    const { id, type, value, color, backgorundColor } = item;
+    return (
+      <button
+        key={id}
+        style={{
+          backgroundColor: techList === type && backgorundColor,
+          color: techList === type ? color : "#9a9a9a",
+        }}
+        onClick={handleTechList}
+        value={type}
+      >
+        {value}
+      </button>
+    );
+  });
   return (
     <article className="main-content">
       <div className="tit_main">
         <img src={kakaoImg} alt="kakao" style={{ width: 200 }} />
         <h3>하루하루 발전하는 삶을 살자</h3>
       </div>
+      <section className="section_main">
+        <div className="artiicle_filter">{buttonRedner}</div>
+      </section>
     </article>
   );
 };
--- src/Utils/TypeManager.js
+++ src/Utils/TypeManager.js
@@ -1,2 +1,55 @@
-const TypeManager = {};
+const mainFilter = [
+  {
+    id: "1",
+    type: "html",
+    value: "html/css",
+    color: "#fff",
+    backgorundColor: "#6e95fd",
+  },
+  {
+    id: "2",
+    type: "javascript",
+    value: "javascript",
+    color: "#fff",
+    backgorundColor: "#6e95fd",
+  },
+  {
+    id: "3",
+    type: "react",
+    value: "react",
+    color: "#fff",
+    backgorundColor: "#6e95fd",
+  },
+  {
+    id: "4",
+    type: "aws",
+    value: "AWS",
+    color: "#fff",
+    backgorundColor: "#6e95fd",
+  },
+  {
+    id: "5",
+    type: "blockchain",
+    value: "블록체인",
+    color: "#fff",
+    backgorundColor: "#6e95fd",
+  },
+];
+
+const TypeManager = {
+  /**
+   * 메인필터 단일 조회
+   * @returns
+   */
+  getMainFilterList: () => {
+    return mainFilter;
+  },
+  getMainFilter: (type) => {
+    const [result] = mainFilter.filter((item) => {
+      const { type: comp } = item;
+      return type === comp;
+    });
+    return result;
+  },
+};
 export default TypeManager;
