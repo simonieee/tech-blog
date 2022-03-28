c867091804e289499236df8472b117296763dc3f
simonieee <75366051+simonieee@users.noreply.github.com>
2022-03-28 17:09:35 +0900

    [UPDATE] contentBox 작성
    contentBox 및 props 작성

--- src/Components/Layout/BlogLayout/Components/Footer.js
+++ src/Components/Layout/BlogLayout/Components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="footer">© Simon. All rights reserved.</div>;
+  return (
+    <div className="footer">
+      <div className="info_copyright">
+        <div className="info_desc">ContactUs: tbspsm@naver.com</div>
+        <div className="info_desc">Copyright @ 2022 Simon.</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
